85c2eb1a9293b320f5b0e52a34a1db9d7c8a7095
Ali Şen <126333294+Alisen3@users.noreply.github.com>
2025-12-02 17:46:20 +0300

    Delete S1-T15-CodeArena-main 3 directory

--- S1-T15-CodeArena-main 3/backend/src/main/java/com/halenteck/demo/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-// Dosya Yolu: demo/QuestionRepository.java
-package com.halenteck.demo.repository;
-
-import com.halenteck.demo.entity.QuestionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {
-    // İleride ihtiyaç olursa buraya özel sorgular eklenebilir.
-}
